ec43e0c024054829f281dc95908878fe4de79eb7
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-14 21:17:29 +0200

    Add create-check.dto + validations

--- src/checks/dto/create-check.dto.ts
+++ src/checks/dto/create-check.dto.ts
@@ -1,1 +1,59 @@
-export class CreateCheckDto {}
+import {
+  IsArray,
+  IsBoolean,
+  IsIn,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsPort,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
+export class CreateCheckDto {
+  @IsString()
+  name: string;
+
+  @IsUrl()
+  url: string;
+
+  @IsIn(['http', 'https', 'tcp'])
+  protocol: string;
+
+  @IsOptional()
+  path?: string;
+
+  @IsOptional()
+  @IsPort()
+  port?: number;
+
+  @IsOptional()
+  @IsString()
+  webhook?: string;
+
+  @IsOptional()
+  @IsNumber()
+  timeout?: number;
+
+  @IsOptional()
+  @IsNumber()
+  interval?: number;
+
+  @IsOptional()
+  @IsNumber()
+  threshold?: number;
+
+  @IsOptional()
+  @IsObject()
+  @IsString({ each: true })
+  authentication?: { username: string; password: string };
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  ignore_ssl?: boolean;
+}
