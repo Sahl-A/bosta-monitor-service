ee8a3a8a29e89e6aecd1e2f30add91a5ef209070
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-16 13:22:14 +0200

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import { Entity, Column, Unique, BeforeInsert, OneToMany } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import Model from '../../shared/database/entity.model';
+import { Check } from '../../checks/entities/check.entity';
+@Entity({ name: 'users' })
+@Unique(['email'])
+export class User extends Model {
+  // one-to-many relation with checks table
+  @OneToMany(() => Check, (check) => check.user)
+  checks: Check[];
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  passwordConfirm: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  // Hide id when fetching from table
+  toJSON() {
+    return { ...this, id: undefined, passwordConfirm: undefined };
+  }
+}
