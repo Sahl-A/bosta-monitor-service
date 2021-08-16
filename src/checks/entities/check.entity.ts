d354f1218c2592a5c130541cdbf657baf2a432f2
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-16 13:22:31 +0200

    add relation between user & check entities

--- src/checks/entities/check.entity.ts
+++ src/checks/entities/check.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, OneToMany } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 
 import Model from '../../shared/database/entity.model';
 import { CheckLog } from './check-log.entity';
@@ -7,4 +8,12 @@ import { CheckLog } from './check-log.entity';
 export class Check extends Model {
   @OneToMany(() => CheckLog, (log) => log.check)
   logs: CheckLog[];
+
+  // many-to-one relation with users
+  @ManyToOne(() => User, (user) => user.checks, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 }
