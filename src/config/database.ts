9e6079fcf5631074dfb7a2fd46d8596212776e9d
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-16 22:35:24 +0200

    update db configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,7 +13,7 @@ const config: TypeOrmModuleOptions = {
   type: 'postgres',
   migrationsRun: true,
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  port: parseInt(process.env.DB_PORT, 10) || 5544,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: 'postgres',
