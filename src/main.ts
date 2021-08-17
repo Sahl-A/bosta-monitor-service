c07ea90bf5ad31dad73efa1fe48acb51d86d0b1f
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-17 17:20:57 +0200

    Add openAPI definitions

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../openAPI/monitor.openAPI.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,9 @@ async function bootstrap() {
 
   // initialize configService to get data from it
   const configService = app.get(ConfigService);
+
+  // get the swagger definitions
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   // get port from configService
   const port = configService.get('port');
